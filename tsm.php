c9ce297840cfae35089f190b42239a576b1692cb
Mohsen Sadeghzade <hi@techiefor.fun>
2019-05-12 01:10:25 +0430

    Update in version number

--- tsm.php
+++ tsm.php
@@ -8,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name:       Teacher's Students Management
  * Description:       Teacher's students management.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Mohsen Sadeghzade
  * Author URI:        https://techiefor.fun/
  * License:           GPL-2.0+
