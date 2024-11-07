a517252b5e42e4c7b063343130ec824a8f34db14
abdolnabi <67148881+abdolnabi@users.noreply.github.com>
2024-11-07 19:15:04 +0800

    feat(index):update texts

--- tsm.php
+++ tsm.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name:       Teacher's Students Management
  * Description:       Teacher's students management.
- * Version:           1.18.0
+ * Version:           1.19.0
  * Author:            Mohsen Sadeghzade
  * Author URI:        https://techiefor.fun/
  * License:           GPL-2.0+
