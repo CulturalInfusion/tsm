0207381001df2871e622fdf4e58d6ea87bf9d9a7
ci-general-account <54678342+ci-general-account@users.noreply.github.com>
2024-11-10 12:21:05 +1100

    feat(index): add more space to email and some typos

--- views/front/index.php
+++ views/front/index.php
@@ -1,25 +1,26 @@
 <link href="<?php echo plugin_dir_url(__DIR__ . '/../../tsm.php') . 'assets/css/style.css'?>" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <div class="tsm-wrapper">
     <h2>
         List of students
     </h2>
-    <a href="<?php echo $this->add_parameters($this->base_url, 'view=create') ?>" title="Add new student" class="button my-10">Add new</a>
-    <a href="<?php echo $this->add_parameters($this->base_url, 'view=import&form=csv') ?>" title="Upload your spreadsheet" class="button my-10">Import from CSV</a>
+    <a href="<?php echo $this->add_parameters($this->base_url, 'view=create') ?>" title="Add new student" class="btn btn-danger">Add new</a>
+    <a href="<?php echo $this->add_parameters($this->base_url, 'view=import&form=csv') ?>" title="Upload your spreadsheet" class="btn btn-danger">Upload your spreadsheet</a>
     <table>
-        <tr>
-            <th>
+        <tr class="h4">
+            <th class="col-sm-1">
                 #
             </th>
-            <th>
+            <th class="col-sm-3">
                 Username
             </th>
-            <th class="col-sm-0">
+            <th class="col-sm-2">
                 Name
             </th>
-            <th class="col-sm-0 email-col">
+            <th class="col-sm-4 email-col">
                 Email
             </th>
-            <th>
+            <th class="col-sm-2">
                 Operations
             </th>
         </tr>
@@ -28,29 +29,29 @@
             $student = get_userdata($student->ID);
             $course = $this->get_student_info($student->ID);
             ?>
-            <tr>
-                <td>
+            <tr class="h4">
+                <td class="col-sm-1">
                     <?php
                     echo $key + 1; ?>
                 </td>
-                <td>
+                <td class="col-sm-3">
                     <?php
                     echo $student->user_login; ?>
                 </td>
-                <td class="col-sm-0">
+                <td class="col-sm-2">
                     <?php
                     echo $student->first_name . ' ' . $student->last_name; ?>
                 </td>
-                <td class="col-sm-0 email-col">
+                <td class="col-sm-4 email-col">
                     <?php
                     echo $student->user_email; ?>
-                <td class="text-center">
-                    <a class="button my-10" href="<?php echo $this->add_parameters($this->base_url, 'view=edit&student_ID=' . $student->ID) ?>">Edit</a>
+                <td class="col-sm-2 text-center">
+                    <a class="button btn btn-danger my-10" href="<?php echo $this->add_parameters($this->base_url, 'view=edit&student_ID=' . $student->ID) ?>">Edit</a>
                     <form id="delete_student_<?php $student->ID ?>" method="post">
                         <?php wp_nonce_field('delete_student'); ?>
                         <input type="hidden" name="task" value="destroy">
                         <input type="hidden" name="student_ID" value="<?php echo $student->ID ?>">
-                        <button class="button" onclick="return confirmAction();">Delete</button>
+                        <button class="button btn btn-danger my-10" onclick="return confirmAction();">Delete</button>
                     </form>
                 </td>
             </tr>
